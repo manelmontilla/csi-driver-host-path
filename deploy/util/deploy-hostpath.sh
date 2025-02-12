03b28237521e19b758c5cf2aa5a1f8db5481d156
Manel Montilla <manel.montilla@gmail.com>
2025-02-12 12:00:44 +0100

    WIP

--- deploy/util/deploy-hostpath.sh
+++ deploy/util/deploy-hostpath.sh
@@ -217,7 +217,7 @@ for component in "${components[@]}"; do
 		cp "${current}" "${TEMP_DIR}"/rbac.yaml
 	fi
 	echo "Installing component ${component}"
-	echo -e "Component content \n: $(cat ${TEMP_DIR}/rbac.yaml)"
+	# echo -e "Component content \n: $(cat ${TEMP_DIR}/rbac.yaml)"
 
 	cat <<-EOF >"${TEMP_DIR}"/kustomization.yaml
 		apiVersion: kustomize.config.k8s.io/v1beta1
